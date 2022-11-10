16e1551df2be8050c441f87f33d6a5eb4607d48b
DavidePistilli173 <davidepistilli173@gmail.com>
2022-11-10 17:23:06 +0100

    Add PipelineLayout.

---
+++ RedWolf/lif/vulkan/PipelineLayout.cpp
@@ -0,0 +1,38 @@
+#include "PipelineLayout.hpp"
+
+#include "RedWolf/RedWolfManager.hpp"
+#include "RedWolf/lif/vulkan/GraphicsDevice.hpp"
+
+using namespace rw::lif::vlk;
+
+PipelineLayout::PipelineLayout(RedWolfManager& manager, GraphicsDevice& device) : BaseObject(manager), device_{ device }
+{
+   VkPipelineLayoutCreateInfo createInfo{};
+   createInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+   createInfo.setLayoutCount = 0U;
+   createInfo.pSetLayouts = nullptr;
+   createInfo.pushConstantRangeCount = 0;
+   createInfo.pPushConstantRanges = nullptr;
+
+   pipelineLayout_ = vulkanInterface_.createPipelineLayout(device_.handle(), createInfo);
+   if (pipelineLayout_ == VK_NULL_HANDLE)
+   {
+      logger_.relFatal("Failed to create pipeline layout.");
+   }
+}
+
+PipelineLayout::~PipelineLayout()
+{
+   vulkanInterface_.destroyPipelineLayout(device_.handle(), pipelineLayout_);
+}
+
+PipelineLayout::PipelineLayout(PipelineLayout&& other) : BaseObject(other.manager_), device_{ other.device_ }
+{
+   pipelineLayout_ = other.pipelineLayout_;
+   other.pipelineLayout_ = VK_NULL_HANDLE;
+}
+
+VkPipelineLayout PipelineLayout::handle()
+{
+   return pipelineLayout_;
+}
---
+++ RedWolf/lif/vulkan/PipelineLayout.hpp
@@ -0,0 +1,70 @@
+#ifndef RW_LIF_VULKAN_PIPELINELAYOUT_HPP
+#define RW_LIF_VULKAN_PIPELINELAYOUT_HPP
+
+#include "RedWolf/common.hpp"
+#include "RedWolf/lif/vulkan/BaseObject.hpp"
+
+namespace rw
+{
+   class RedWolfManager;
+}
+
+namespace rw::lif::vlk
+{
+   class GraphicsDevice;
+}
+
+namespace rw::lif::vlk
+{
+   /**
+    * @brief Wrapper for a Vulkan VkPipelineLayout object.
+    */
+   class RW_API PipelineLayout : public BaseObject
+   {
+   public:
+      /**
+       * @brief Constructor.
+       * @param manager RedWolf library manager.
+       * @param device Graphics device that will use this pipeline layout.
+       */
+      PipelineLayout(RedWolfManager& manager, GraphicsDevice& device);
+
+      /**
+       * @brief Destructor.
+       */
+      ~PipelineLayout();
+
+      /**
+       * @brief Copy constructor.
+       */
+      PipelineLayout(const PipelineLayout&) = delete;
+
+      /**
+       * @brief Move constructor.
+       */
+      PipelineLayout(PipelineLayout&& other);
+
+      /**
+       * @brief Copy-assignment operator.
+       */
+      PipelineLayout& operator=(const PipelineLayout&) = delete;
+
+      /**
+       * @brief Move-assignment operator.
+       */
+      PipelineLayout& operator=(PipelineLayout&&) = delete;
+
+      /**
+       * @brief Get the raw pipeline layout handle.
+       * @return Raw pipeline layout handle.
+       */
+      [[nodiscard]] VkPipelineLayout handle();
+
+   private:
+      VkPipelineLayout pipelineLayout_{ VK_NULL_HANDLE }; /**< Raw handle to the pipeline layout. */
+
+      GraphicsDevice& device_; /**< Graphics device this pipeline layout is bound to. */
+   };
+} // namespace rw::lif::vlk
+
+#endif
