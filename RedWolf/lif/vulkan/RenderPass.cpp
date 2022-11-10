97139d1f56f811befaafb86839d603311940bccb
DavidePistilli173 <davidepistilli173@gmail.com>
2022-11-10 17:22:54 +0100

    Add RenderPass.

---
+++ RedWolf/lif/vulkan/RenderPass.cpp
@@ -0,0 +1,54 @@
+#include "RenderPass.hpp"
+
+#include "RedWolf/RedWolfManager.hpp"
+#include "RedWolf/lif/vulkan/GraphicsDevice.hpp"
+#include "RedWolf/lif/vulkan/SwapChain.hpp"
+
+using namespace rw::lif::vlk;
+
+RenderPass::RenderPass(RedWolfManager& manager, GraphicsDevice& device, SwapChain& swapChain) :
+   BaseObject(manager), graphicsDevice_{ device }
+{
+   VkAttachmentDescription colourAttachmentDescription{};
+   colourAttachmentDescription.format = swapChain.format().format;
+   colourAttachmentDescription.samples = VK_SAMPLE_COUNT_1_BIT;
+   colourAttachmentDescription.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+   colourAttachmentDescription.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+   colourAttachmentDescription.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+   colourAttachmentDescription.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+   colourAttachmentDescription.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+   colourAttachmentDescription.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+   VkAttachmentReference colourAttachmentRef{};
+   colourAttachmentRef.attachment = 0U;
+   colourAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+   VkSubpassDescription subpassDescription{};
+   subpassDescription.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+   subpassDescription.colorAttachmentCount = 1U;
+   subpassDescription.pColorAttachments = &colourAttachmentRef;
+
+   VkRenderPassCreateInfo renderPassInfo{};
+   renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+   renderPassInfo.attachmentCount = 1U;
+   renderPassInfo.pAttachments = &colourAttachmentDescription;
+   renderPassInfo.subpassCount = 1U;
+   renderPassInfo.pSubpasses = &subpassDescription;
+
+   renderPass_ = vulkanInterface_.createRenderPass(graphicsDevice_.handle(), renderPassInfo);
+   if (renderPass_ == VK_NULL_HANDLE)
+   {
+      logger_.relFatal("Failed to create render pass.");
+   }
+}
+
+RenderPass::~RenderPass()
+{
+   vulkanInterface_.destroyRenderPass(graphicsDevice_.handle(), renderPass_);
+}
+
+RenderPass::RenderPass(RenderPass&& other) : BaseObject(other.manager_), graphicsDevice_{ other.graphicsDevice_ }
+{
+   renderPass_ = other.renderPass_;
+   other.renderPass_ = VK_NULL_HANDLE;
+}
---
+++ RedWolf/lif/vulkan/RenderPass.hpp
@@ -0,0 +1,66 @@
+#ifndef RW_LIF_VULKAN_RENDERPASS_HPP
+#define RW_LIF_VULKAN_RENDERPASS_HPP
+
+#include "RedWolf/common.hpp"
+#include "RedWolf/lif/vulkan/BaseObject.hpp"
+
+namespace rw
+{
+   class RedWolfManager;
+}
+
+namespace rw::lif::vlk
+{
+   class GraphicsDevice;
+   class SwapChain;
+} // namespace rw::lif::vlk
+
+namespace rw::lif::vlk
+{
+   /**
+    * @brief Wrapper for a Vulkan VkRenderPass object.
+    */
+   class RW_API RenderPass : public BaseObject
+   {
+   public:
+      /**
+       * @brief Constructor.
+       * @param manager RedWolf library manager.
+       * @param device Graphics device this render pass will be used on.
+       * @param swapChain Swap chain this render pass will be used on.
+       */
+      RenderPass(RedWolfManager& manager, GraphicsDevice& device, SwapChain& swapChain);
+
+      /**
+       * @brief Destructor.
+       */
+      ~RenderPass();
+
+      /**
+       * @brief Copy constructor.
+       */
+      RenderPass(const RenderPass&) = delete;
+
+      /**
+       * @brief Move constructor.
+       */
+      RenderPass(RenderPass&& other);
+
+      /**
+       * @brief Copy-assignment operator.
+       */
+      RenderPass& operator=(const RenderPass&) = delete;
+
+      /**
+       * @brief Move-assignment operator.
+       */
+      RenderPass& operator=(RenderPass&&) = delete;
+
+   private:
+      VkRenderPass renderPass_{ VK_NULL_HANDLE }; /**< Raw handle to the render pass. */
+
+      GraphicsDevice& graphicsDevice_; /**< Graphics device this render pass is bound to. */
+   };
+} // namespace rw::lif::vlk
+
+#endif
