4aeda9ec5233b175ab2098fd0961e6b1b58d7a91
PISTILLID <davide.pistilli@leonardo.com>
2023-05-19 17:37:22 +0200

    Refactoring, adding tests.

--- RedWolf/lif/vulkan/PhysicalDevice.cpp
+++ RedWolf/lif/vulkan/PhysicalDevice.cpp
@@ -15,9 +15,51 @@ PhysicalDevice::PhysicalDevice(rw::util::Logger& logger, VkPhysicalDevice physic
    vkGetPhysicalDeviceFeatures(physicalDevice_, &features_);
    vkGetPhysicalDeviceMemoryProperties(physicalDevice_, &memoryProperties_);
 
+   u32      count{ 0U };
+   VkResult errCode{ vkEnumerateDeviceExtensionProperties(physicalDevice_, nullptr, &count, nullptr) };
+   if (errCode == VK_SUCCESS && count > 0U)
+   {
+      extensionProperties_.resize(count);
+      errCode = vkEnumerateDeviceExtensionProperties(physicalDevice_, nullptr, &count, extensionProperties_.data());
+      if (errCode != VK_SUCCESS)
+      {
+         extensionProperties_.resize(0U);
+      }
+   }
+
    // Initialise supported queues.
    u32 queueFamilyCount{ 0U };
    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice_, &queueFamilyCount, nullptr);
    queueFamilyProperties_.resize(queueFamilyCount);
    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice_, &queueFamilyCount, queueFamilyProperties_.data());
+}
+
+const std::vector<VkExtensionProperties>& PhysicalDevice::extensionProperties() const
+{
+   return extensionProperties_;
+}
+
+const VkPhysicalDeviceFeatures& PhysicalDevice::features() const
+{
+   return features_;
+}
+
+VkPhysicalDevice PhysicalDevice::handle() const
+{
+   return physicalDevice_;
+}
+
+const VkPhysicalDeviceMemoryProperties& PhysicalDevice::memoryProperties() const
+{
+   return memoryProperties_;
+}
+
+const VkPhysicalDeviceProperties& PhysicalDevice::properties() const
+{
+   return properties_;
+}
+
+const std::vector<VkQueueFamilyProperties> PhysicalDevice::queueFamilyProperties() const
+{
+   return queueFamilyProperties_;
 }
--- RedWolf/lif/vulkan/PhysicalDevice.hpp
+++ RedWolf/lif/vulkan/PhysicalDevice.hpp
@@ -22,6 +22,41 @@ namespace rw::lif::vulkan
        */
       PhysicalDevice(rw::util::Logger& logger, VkPhysicalDevice physicalDevice);
 
+      /**
+       * @brief Get the properties of the extensions supported by the physical device.
+       * @return Properties of the extensions supported by the physical device.
+       */
+      [[nodiscard]] const std::vector<VkExtensionProperties>& extensionProperties() const;
+
+      /**
+       * @brief Get the features of the physical device.
+       * @return Features of the physical device.
+       */
+      [[nodiscard]] const VkPhysicalDeviceFeatures& features() const;
+
+      /**
+       * @brief Get the device handle.
+       * @return Handle of the physical device.
+       */
+      [[nodiscard]] VkPhysicalDevice handle() const;
+
+      /**
+       * @brief Get the memory properties of the device.
+       */
+      [[nodiscard]] const VkPhysicalDeviceMemoryProperties& memoryProperties() const;
+
+      /**
+       * @brief Get the properties of a physical device.
+       * @return Properties of the physical device.
+       */
+      [[nodiscard]] const VkPhysicalDeviceProperties& properties() const;
+
+      /**
+       * @brief Get the properties of all supported queue families.
+       * @return Properties of all supported queue families.
+       */
+      [[nodiscard]] const std::vector<VkQueueFamilyProperties> queueFamilyProperties() const;
+
    private:
       rw::util::Logger& logger_; /**< Logger. */
 
@@ -29,6 +64,7 @@ namespace rw::lif::vulkan
       VkPhysicalDeviceProperties           properties_;                       /**< Physical device properties. */
       VkPhysicalDeviceFeatures             features_;                         /**< Physical device features. */
       VkPhysicalDeviceMemoryProperties     memoryProperties_;                 /**< Physical device memory properties. */
+      std::vector<VkExtensionProperties>   extensionProperties_;              /**< Physical device extension properties. */
       std::vector<VkQueueFamilyProperties> queueFamilyProperties_; /**< Properties of all queue families supported by this device. */
    };
 } // namespace rw::lif::vulkan
